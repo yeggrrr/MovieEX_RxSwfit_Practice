af8e9e1053a30df2a540dc3754e3bd5187a10e82
yeggrrr <yjkim981103@gmail.com>
2024-08-08 13:44:19 +0900

    Initial Commit

---
+++ MovieEX_RxSwfit_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieEX_RxSwfit_Practice
+//
+//  Created by YJ on 8/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
